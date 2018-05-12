2c06645c1963fcf7f0647dd16e40dbe581384ae9
shelltr <shelltr.me@gmail.com>
2018-05-11 21:01:52 -0700

    update contracts [wip]

--- michelle/contracts/Owned.sol
+++ michelle/contracts/Owned.sol
@@ -1,17 +1,17 @@
 pragma solidity ^0.4.11;
 
 contract Owned {
-  // State variables
-  address owner;
+    // State variables
+    address owner;
 
-  //modifiers
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
+    //modifiers
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
-  // constructor
-  function Owned(){
-    owner = msg.sender;
-  }
+    // constructor
+    function Owned() public {
+        owner = msg.sender;
+    }
 }
