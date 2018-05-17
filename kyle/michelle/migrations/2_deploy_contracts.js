80b1318517c4da4fe8b5edd6b52df19a45812be0
Kyle <k.yasumiishi@gmail.com>
2018-05-17 12:07:30 -0700

    Copy and pasted michelle's folder to kyle's folder in kyle's branch

---
+++ kyle/michelle/migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+// var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+// var LyrifyTokenFactory = artifacts.require("./LyrifyTokenFactory.sol");
+var LyrifyToken = artifacts.require("./LyrifyTokenOwnership.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(LyrifyToken);
+};
