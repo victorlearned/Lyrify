f7c7b6d39e2e13de34106cbbfc318a76553849c0
Kyle <k.yasumiishi@gmail.com>
2018-05-20 14:05:10 -0700

    Fixed bug in app.js by changing web3 to window.web3, which makes MetaMask inject web3 to the Chrome browser to approve transactions.

--- kyle/src/js/app.js
+++ kyle/src/js/app.js
@@ -3,7 +3,6 @@ document.addEventListener("load", componentWillMount);
 // Global variables (similar to properties in a React component's state object).
 let allTokens = null;
 let ownedTokens = null;
-let web3;
 let account = null;
 let lyrifyInstance = null;
 let submission = {
@@ -17,10 +16,13 @@ let submission = {
  */
 function componentWillMount() {
     if (typeof web3 !== 'undefined') {
-        web3 = new Web3(web3.currentProvider);
+        window.web3 = new Web3(web3.currentProvider);
     } else {
-        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        console.log('MetaMask failed to inject web3');
+        window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
     }
+    console.log("web3: ", web3);
+    console.log("web3.eth: ", web3.eth);
     instantiateContract();
 };
 
@@ -36,11 +38,14 @@ function instantiateContract() {
    
         // Get accounts - why?
         web3.eth.getAccounts((error, accounts) => {
+            console.log("web3.eth.getAccounts: ", accounts);
             account = accounts[0];
+            console.log("account: ", account);
         });
     
         // Set default account - why?
         web3.eth.defaultAccount = web3.eth.accounts[0];
+        console.log("web3.eth", web3.eth);
     
         // Create instance of contract at its deployed address (https://github.com/trufflesuite/truffle-contract).
         lyrifyContract.deployed().then(function(instance) {
@@ -82,19 +87,27 @@ function submitHandler(event) {
     submission.lyrics = document.getElementById("lyrics").value;
     alert('Your lyrics were submitted! ' + JSON.stringify(submission));
     event.preventDefault();
-    console.log(lyrifyInstance);
+    console.log("lyrify instance??", account);
+
+
     return lyrifyInstance.registerToken(submission.ownerName, submission.songTitle, submission.lyrics, {
         from: account,
         value: web3.toWei(0.004, "ether"), // hardcoded value
         gas: 500000
-    }).then((error, result) => {
-        if (error) {
-            console.log(error);
-        } else {
-            alert('Token has been registered!');
-            console.log(result);
-        }
-    });
+    }).then((result) => {
+        console.log("registering token:", result);
+    })
+    
+    
+    // .then((error, result) => {
+    //     if (error) {
+    //         console.log(error);
+    //     } else {
+    //         alert('Token has been registered!');
+    //         console.log(result);
+    //     }
+    // });
+    console.log(lyrifyInstance);
 };
 
 
