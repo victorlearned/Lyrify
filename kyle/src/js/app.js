d8006a61449a6534b37a0bf08177bf669a4dfaa3
Kyle <k.yasumiishi@gmail.com>
2018-05-21 12:36:36 -0700

    Cleaned up form by replacing password field with song title field. Changed onclick to onsubmit so that form required attributes work

--- kyle/src/js/app.js
+++ kyle/src/js/app.js
@@ -1,4 +1,5 @@
 document.addEventListener("load", componentWillMount);
+document.addEventListener("submit", submitHandler);
 
 // Global variables (similar to properties in a React component's state object).
 let allTokens = null;
@@ -21,8 +22,6 @@ function componentWillMount() {
         console.log('MetaMask failed to inject web3');
         window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
     }
-    console.log("web3: ", web3);
-    console.log("web3.eth: ", web3.eth);
     instantiateContract();
 };
 
@@ -36,22 +35,20 @@ function instantiateContract() {
         const lyrifyContract = TruffleContract(LyrifyTokenOwnership);
         lyrifyContract.setProvider(web3.currentProvider);
    
-        // Get accounts - why?
+        // Get accounts
         web3.eth.getAccounts((error, accounts) => {
-            console.log("web3.eth.getAccounts: ", accounts);
             account = accounts[0];
             console.log("account: ", account);
         });
     
-        // Set default account - why?
+        // Set default account
         web3.eth.defaultAccount = web3.eth.accounts[0];
-        console.log("web3.eth", web3.eth);
     
         // Create instance of contract at its deployed address (https://github.com/trufflesuite/truffle-contract).
         lyrifyContract.deployed().then(function(instance) {
             lyrifyInstance = instance;
             getTokens().then(result => {
-                console.log("alltokens", result);
+                console.log("allTokens: ", result);
                 allTokens = result;
             }); 
         });
@@ -85,7 +82,6 @@ function submitHandler(event) {
     submission.email = document.getElementById("email").value;
     submission.songTitle = document.getElementById("title").value;
     submission.lyrics = document.getElementById("lyrics").value;
-    alert('Your lyrics were submitted! ' + JSON.stringify(submission));
     event.preventDefault();
     return registerToken();
 };
@@ -99,12 +95,12 @@ function registerToken() {
         value: web3.toWei(0.004, "ether"), // hardcoded value
         gas: 999999 // need to optimize this
     }).then((result) => {
-        console.log("registering token:", result);
+        console.log("registered token: ", result);
         let submissionConfirmation = JSON.stringify(result.logs[0].args);
         let transactionHash = JSON.stringify(result.tx)
         alert("registered token: " + submissionConfirmation + transactionHash);
         getTokens().then(result => {
-            console.log("alltokens", result);
+            console.log("allTokens: ", result);
             allTokens = result;
         });
     });
@@ -143,5 +139,3 @@ function getTokens() {
             return Promise.all(promises);
         });
 }
-
-
